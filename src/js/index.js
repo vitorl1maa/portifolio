cc722e370c53523ed4306c3ea5f44544304c21c3
vitorl1ma <vitornascimento923@gmail.com>
2022-12-01 23:13:06 -0300

    full application

---
+++ src/js/index.js
@@ -0,0 +1,64 @@
+const cards = document.querySelectorAll('.content-card');
+
+cards.forEach(card => {
+  const contentText = card.querySelector('.content-text');
+  const cardImage = card.querySelector('.content-card-img');
+  
+  const informationBtn = card.querySelector('.information-btn');
+  informationBtn.addEventListener('click', () => {
+    contentText.classList.remove('hidden');
+    cardImage.style.display = 'none';
+  });
+
+  const backBtn = card.querySelector('.back-button');
+  backBtn.addEventListener('click', () => {
+    contentText.classList.add('hidden');
+    cardImage.style.display = 'block';
+  });
+});
+
+const salutation = document.querySelector('.salutation');
+
+function typeWrite(element) {
+  const textArray = element.innerHTML.split('');
+  element.innerHTML = ''
+  console.log(textArray)
+  textArray.forEach((letter, i) => {
+    setTimeout(() => element.innerHTML += letter, 75 * i);
+  });
+}
+typeWrite(salutation);
+
+const menuIcon = document.querySelector('#menu-icon');
+
+function toggleMenu(event) {
+  if (event.type === 'touchstart') event.preventDefault()
+  const nav = document.querySelector('#nav');
+  nav.classList.toggle('active');
+  const active = nav.classList.contains('active');
+  event.currentTarget.setAttribute('aria-expanded', active);
+  if (active) {
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+
+  } else {
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+  }
+  
+};
+
+menuIcon.addEventListener('click', toggleMenu);
+menuIcon.addEventListener('touchstart', toggleMenu);
+
+window.onscroll = () => {
+  nav.classList.remove('active');
+};
+
+const scrollAnimation = ScrollReveal ({
+  distance: '60px',
+  duration: 2500,
+  reset: true
+});
+
+scrollAnimation.reveal('.home-img', {delay:400, origin:'top'});
+scrollAnimation.reveal('.home-img', {delay:400, origin:'top'});
+scrollAnimation.reveal('.about, .projects, .other-projects, .cta, .resume, .contact', {delay: 200, origin:'top'} );
